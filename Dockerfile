255be6465276f7d1bf40af0fce3ae0733162811b
Sharma <jitendra_sharma@condenast.com>
2022-12-18 15:05:18 +0530

    Env variables

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,3 @@
 FROM quay.io/astronomer/astro-runtime:7.0.0
+
+ENV AIRFLOW_VAR_MY_DATA ='{"name":"Sam", "address":"uk"}'
--- dags/basic/variable.py
+++ dags/basic/variable.py
@@ -5,11 +5,17 @@ from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 
 default_args= {'owner':'jsharma',
-    'email':'my@gmail.com'}
+    'email':'my@gmail.com',
+    'retries': 1}
 
-def var_task_fun():
+def var_task_fun(name):
     param= Variable.get('my_param')
-    print("My variable paramter",param)
+    #param_docker= Variable.get('MY_DATA',deserialize_json=True)
+    #print('From docker env file',param_docker['name'])
+    # To fetch jason value
+    param_json= Variable.get('my_param_json', deserialize_json=True)
+    print("My variable paramter",param_json['address'])
+    print("From function param",name)
 
 with DAG(dag_id="varible_demo",
     default_args=default_args,
@@ -24,7 +30,8 @@ with DAG(dag_id="varible_demo",
     
     my_var_task = PythonOperator(
         task_id='var_task',
-        python_callable=var_task_fun
+        python_callable=var_task_fun,
+        op_args = ["{{ var.json.my_param_json.name }}"]
     )
 
     dummy_task = DummyOperator(task_id="dummy_task")
